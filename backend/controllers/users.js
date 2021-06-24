4da97b1c9ce91e3b32584729cdbd8dfffb2b13ab
Sergey Ignatov <sergbfmv@yandex.ru>
2021-06-24 20:13:16 +0300

    15 project after first review

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -134,15 +134,4 @@ module.exports.updateAvatar = (req, res, next) => {
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   console.log('jkk');
-  return User.findUserByCredentials(email, password)
-    .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
-      res.send({ token });
-      next();
-    })
-    .catch(() => {
-      const error = new AuthError('Невозможно авторизоваться');
-      next(error);
-    })
-    .catch(next);
 };
